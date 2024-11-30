ff8bfc3038c423518ec673008d7cb0fa124d2bc4
Hashir416 <msheikhashir@gmail.com>
2024-11-29 21:08:33 -0500

    Updated app.js and config.js

--- public/Script/app.js
+++ public/Script/app.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-// Immediately Invoked Function Expression (IIFE) for safe code execution
-(function() {
-    function start() {
-        console.log("App started successfully");
-    }
-    window.addEventListener("load", start);
-})();
-=======
 // immediately invoked function expression
-
 (function(){
     function start(){
         console.log("app started")
     }
-    window.addEventListener("load",start);
+    window.addEventListener("load", start);
 })();
->>>>>>> 6569a9bc24b022a06f6df1da29a05894dbc3f927
+
--- server/config/app.js
+++ server/config/app.js
@@ -6,19 +6,12 @@ let logger = require('morgan');
 let session = require('express-session');
 let passport = require('passport');
 let passportlocal = require('passport-local');
-<<<<<<< HEAD
-=======
 let localtrategy = passportlocal.Strategy;
->>>>>>> 6569a9bc24b022a06f6df1da29a05894dbc3f927
 let flash = require('connect-flash');
 
 let app = express();
 let indexRouter = require('../routes/index');
-<<<<<<< HEAD
-let TBMRouter = require('../routes/TBM');
-=======
 let TBMRouter = require('../routes/TBM'); // Updated from WtrackerRouter to TBMRouter
->>>>>>> 6569a9bc24b022a06f6df1da29a05894dbc3f927
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
@@ -27,10 +20,7 @@ app.set('view engine', 'ejs');
 // MongoDB setup
 const mongoose = require('mongoose');
 let DB = require('./db');
-<<<<<<< HEAD
-=======
 // Point mongoose to the DB URI
->>>>>>> 6569a9bc24b022a06f6df1da29a05894dbc3f927
 mongoose.connect(DB.URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let mongoDB = mongoose.connection;
 mongoDB.on('error', console.error.bind(console, 'Connection Error'));
@@ -38,9 +28,6 @@ mongoDB.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-<<<<<<< HEAD
-// Middleware
-=======
 app.use(session({
   secret: "SomeSecret",
   saveUninitialized: false,
@@ -51,7 +38,6 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
->>>>>>> 6569a9bc24b022a06f6df1da29a05894dbc3f927
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -59,24 +45,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
 
-<<<<<<< HEAD
-app.use(session({
-  secret: "SomeSecret",
-  saveUninitialized: false,
-  resave: false
-}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Routes
-app.use('/', indexRouter);
-app.use('/tournaments', TBMRouter);
-=======
 // Define routes
 app.use('/', indexRouter); // Home page
 app.use('/tournaments', TBMRouter); // Tournaments page
->>>>>>> 6569a9bc24b022a06f6df1da29a05894dbc3f927
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -85,24 +56,14 @@ app.use(function (req, res, next) {
 
 // Error handler
 app.use(function (err, req, res, next) {
-<<<<<<< HEAD
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-=======
   // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // Render the error page
->>>>>>> 6569a9bc24b022a06f6df1da29a05894dbc3f927
   res.status(err.status || 500);
   res.render('error', { title: 'Error' });
 });
 
 module.exports = app;
-<<<<<<< HEAD
-=======
 
-
-
->>>>>>> 6569a9bc24b022a06f6df1da29a05894dbc3f927
