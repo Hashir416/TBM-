359721fb8912d71256325f04ee6751b675d9d493
Muneeb Mahmood <muneeb.mahmood@ontariotechu.net>
2024-11-29 20:59:38 -0500

    Updated files and fixed issues

--- public/Script/app.js
+++ public/Script/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Immediately Invoked Function Expression (IIFE) for safe code execution
 (function() {
     function start() {
@@ -6,13 +5,3 @@
     }
     window.addEventListener("load", start);
 })();
-=======
-// immediately invoked function expression
-
-(function(){
-    function start(){
-        console.log("app started")
-    }
-    window.addEventListener("load",start);
-})();
->>>>>>> 6569a9bc24b022a06f6df1da29a05894dbc3f927
